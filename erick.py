9221500ab27baf35fce32249eef2819abda7db11
erfrmnsyh <113223326+erfrmnsyh@users.noreply.github.com>
2022-09-10 10:29:44 +0700

    Add files via upload

---
+++ erick.py
@@ -0,0 +1,46 @@
+def fungsi_phyton():
+    print("hai ini adalah phyton")
+
+nama ="erick firmansyah"
+umur = 16
+ukuransepatu = 42.5
+print(" nama saya adalah " +nama + " umur saya adalah " + str(umur))
+print("umur saya adalah :",umur)
+print(ukuransepatu, ":dia adalah",type(ukuransepatu))
+
+
+
+
+# boolean = 
+masukkelas = True
+pulangkelas =False
+
+
+
+
+# List
+hewan = ["Ayam", "kucing", "kelinci", "tupai"]
+print(hewan)
+
+
+
+
+# boolean = 
+masukkelas = True
+pulangkelas = False
+
+
+fungsi_phyton()
+    # ini komentar
+    # ini bukan komentar
+    # ctrl+/
+
+angka1 = 5
+angka2 = 10
+angka3 = 15
+print( "penjumlahan :" + str(angka1 + angka2 + angka3 + 15))
+print( "pengurangan :" + str(angka1 - angka2 - angka3 - 15))
+print( "pembagian :" + str(angka1/ angka2))
+print( "pembagian 2 :" + str(angka2/ angka1))
+print( "perkalian : " +str(angka1*angka2*10))
+print( "modulus : " , 70%10)
