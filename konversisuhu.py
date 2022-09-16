5558421b9a800191d38ad1330f4b835431013b60
erfrmnsyh <113223326+erfrmnsyh@users.noreply.github.com>
2022-09-16 10:16:37 +0700

    Add files via upload

---
+++ konversisuhu.py
@@ -0,0 +1,17 @@
+#menguah dari celcius ke farenheit
+celcius= int(input("masukan celcius= "))
+farenheit= (9/5*(celcius))+32
+print(farenheit,"f")
+
+celcius= int(input("masukkan celcius="))
+kelvin= celcius+273.15
+print(kelvin,"k")
+
+kelvin= int(input("masukkan kelvin="))
+farenheit= (kelvin-273.15)*9/5+32
+print(farenheit,"f")
+
+reamur= int(input("masukkan reamur="))
+celcius= (5/4)*reamur
+print(celcius,"c")
+
